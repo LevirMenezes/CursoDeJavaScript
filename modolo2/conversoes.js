13f5a49a47aa670a67b987bde0c4009fcb03159a
Levir Menezes <levirmenezes@gmail.com>
2023-03-06 15:02:20 -0400

    aprendendo mais sobre tipos e converções em JavaScript

---
+++ modolo2/conversoes.js
@@ -0,0 +1,18 @@
+//tipo de dado
+// booleanos
+
+//conversão implícita
+
+const numero = 456;
+const numeroString = "456a";
+
+console.log(numero  +  numeroString);
+
+console.log(numero == numeroString)
+
+// Number()
+//String()
+// conversão explícita
+
+console.log(numero + Number(numeroString))
+
---
+++ modolo2/truthy-falsy.js
@@ -0,0 +1,29 @@
+// boolean
+
+const usuarioLogado = true;
+const contapaga = false;
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+// null
+// undefined
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+let minhaVar;
+let varNull = null;
+console.log(typeof minhaVar);//undefined
+console.log(typeof varNull);//object
+
+
