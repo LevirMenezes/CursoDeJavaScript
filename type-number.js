8ffe23c974939337954ce1086b2a55d332bc8385
Levir Menezes <levirmenezes@gmail.com>
2023-03-06 14:03:49 -0400

    exemplos da aulla1

---
+++ type-number.js
@@ -0,0 +1,19 @@
+// tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatemarica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatemarica)
+
+
+// ponto flutuante = 3.3;
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
